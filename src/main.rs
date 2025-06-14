8481f3051b5a1b1bbb7e1a362bf2272ed9feb330
Bruno Bento <brunobbdearaujo@gmail.com>
2025-06-14 18:14:56 -0300

    create asset initial definition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod asset;
+mod traits;
+
+use asset::Asset;
+use traits::Executable;
+
 fn main() {
-    println!("Hello, world!");
+    // Create an asset with a more complex closure
+    let complex_asset = Asset::new(
+        "Complex Asset".to_string(),
+        vec!["complex".to_string()],
+        "Performs calculation".to_string(),
+        || {
+            let x = 10;
+            let y = 5;
+            x * y + 2
+        },
+    );
+    println!("Complex asset result: {}", complex_asset.execute());
 }
---
+++ src/traits.rs
@@ -0,0 +1,14 @@
+//! Contains traits that define common behaviors across the system.
+
+/// The `Executable` trait defines types that can be executed to produce a result.
+///
+/// This trait is designed to be implemented by any type that can perform
+/// some kind of operation when requested and return a result of any type.
+pub trait Executable<R> {
+    /// Execute the implementing type and produce a result of type `R`.
+    ///
+    /// # Returns
+    ///
+    /// A value of type `R` that represents the result of execution.
+    fn execute(&self) -> R;
+}
