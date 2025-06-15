552f6b8b599ea8bc537d53b3dab37643d7c9a41c
Bruno Bento <37910251+brunobbaraujo@users.noreply.github.com>
2025-06-15 16:17:34 -0300

    change folder structure to workspace

---
+++ migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,29 @@
+use psykan::models::Runs;
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Runs::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Runs::Id))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .drop_table(Table::drop().table(Runs::Table).to_owned())
+            .await
+    }
+}
